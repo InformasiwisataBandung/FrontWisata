01c12ca057aa99859bd2a7dc225eaaf52264d397
ShintaRaudita <noctua1530@gmail.com>
2023-10-25 12:35:28 +0700

    login fix

--- js/login.js
+++ js/login.js
@@ -3,6 +3,7 @@ const loginForm = document.getElementById("loginForm");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const submitButton = document.getElementById("submit");
+const errorMessage = document.getElementById("error-message"); //ini
 
 //Fungsi untuk mengecek apakah form telah diisi dengan benar 
 const validation = () => {
@@ -19,6 +20,38 @@ const validation = () => {
 usernameInput.addEventListener("input", validation);
 passwordInput.addEventListener("input", validation);
 
+loginForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // Kirim permintaan HTTP POST ke server Golang (sesuaikan dengan URL yang benar)
+    fetch("https://asia-southeast2-testlogin-366704.cloudfunctions.net/function-16", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === true) {
+                // Redirect user to user.html upon successful login
+                window.location.href = "../pages/user.html";
+            } else {
+                // Handle failed login
+                errorMessage.textContent = "User not found"; // Tambahkan pesan kesalahan
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+});
+
+
+
+
+/**
 // Handle form submission
 loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -42,3 +75,4 @@ loginForm.addEventListener("submit", async (event) => {
         console.log("Login failed. Please check your username and password.");
     }
 });
+ */
--- js/notfound.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', function (event) {
-  const torch = document.querySelector('.torch');
-  torch.style.top = event.clientY - 1 + 'px';
-  torch.style.left = event.clientX - 1 + 'px';
-});
